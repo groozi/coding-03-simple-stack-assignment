0a2fadbb54fa252cc09a733577c97d03709076a4
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-13 03:46:42 -0600

    Modified code in main.cpp to test if stack functions correctly

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@
 int main(int argc, char** argv){
 	std::cout << "Hello, World!" << std::endl;
 
-	Stack s1;
-	s1.push(10);
-	s1.push(100);
+	Stack stack1();
+	stack1.push(12);
+	std::cout << stack1.isEmpty() << std::endl;
+	stack1.peek();
+	stack1.push(10);
+	stack1.peek();
+	stack1.pop();
+	stack1.peek();
 
     return 0;
 }
